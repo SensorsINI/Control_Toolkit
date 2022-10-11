16e209995145b1a318e6c618be34a19ab0e6eaae
heetmeyer <heerfn@yahoo.de>
2022-10-11 14:43:54 +0200

    - Get max. entropy RPGD to work on envs with >1 actions - Add uniform sampling to zeta() to test the difference

--- Controllers/controller_rpgd_me_tf.py
+++ Controllers/controller_rpgd_me_tf.py
@@ -5,6 +5,7 @@ from Control_Toolkit.Cost_Functions import cost_function_base
 from Control_Toolkit.others.globals_and_utils import Compile, get_logger
 from gym.spaces.box import Box
 from SI_Toolkit.Predictors import predictor
+import tensorflow_probability as tfp
 
 logger = get_logger(__name__)
 
@@ -56,6 +57,7 @@ class controller_rpgd_me_tf(template_controller):
         self.gradmax_clip = tf.constant(gradmax_clip, dtype=tf.float32)
         self.rtol = rtol
         self.maximum_entropy_alpha = maximum_entropy_alpha
+        self.gaussian = tfp.distributions.Normal(loc=0., scale=1.)
 
         # Warmup setup
         self.first_iter_count = self.outer_its
@@ -99,22 +101,35 @@ class controller_rpgd_me_tf(template_controller):
             epsilon=adam_epsilon,
         )
         
-        self.theta_min, self.theta_max = tf.constant([[[float(self.action_low), 0.01]]]), tf.constant([[[float(self.action_high), 10.0]]])
+        # Theta bound has dimension (num_actions, num_params_per_action)
+        # Example: Environment with 3 control inputs and U[a,b] sampling => (3, 2)
+        # self.theta_min, self.theta_max = tf.constant([[[float(self.action_low), 0.01]]]), tf.constant([[[float(self.action_high), 10.0]]])
+        # self.theta_min, self.theta_max = tf.constant([[[float(self.action_low), float(self.action_low)]]]), tf.constant([[[float(self.action_high), float(self.action_high)]]])
+        self.theta_min = tf.repeat(tf.expand_dims(self.action_low, 1), 2, 1)
+        self.theta_max = tf.repeat(tf.expand_dims(self.action_high, 1), 2, 1)
         
         self.controller_reset()
     
     def zeta(self, theta: tf.Variable, epsilon: tf.Tensor):
         """Corresponds to N(mu, stdev) with each sample independent."""
-        mu, stdev = (tf.expand_dims(v, -1) for v in tf.unstack(theta, 2, -1))
-        Q = mu + stdev * epsilon
+        # mu, stdev = (tf.expand_dims(v, -1) for v in tf.unstack(theta, 2, -1))
+        # Q = mu + stdev * epsilon
+        # Q_clipped = tf.clip_by_value(Q, self.action_low, self.action_high)
+        # return Q_clipped
+    
+        l, r = tf.unstack(theta, 2, -1)
+        Q = (r - l) * self.gaussian.cdf(epsilon) + l
         Q_clipped = tf.clip_by_value(Q, self.action_low, self.action_high)
         return Q_clipped
+        
 
     def entropy(self, theta):
         """Computes the Shannon entropy of a univariate Gaussian N(mu, sigma). theta = [mu, sigma].
         See https://gregorygundersen.com/blog/2020/09/01/gaussian-entropy/"""
-        stdev = theta[..., 1:]
-        return 0.5 * tf.math.log(2 * np.pi * stdev**2) + 0.5
+        # stdev = theta[..., 1:]
+        # return 0.5 * tf.math.log(2 * np.pi * stdev**2) + 0.5
+        l, r = tf.unstack(theta, 2, -1)
+        return tf.math.log(tf.maximum(r - l, 1e-8))
         
     @Compile
     def sample_actions(self, rng_gen: tf.random.Generator, batch_size: int):
@@ -147,10 +162,10 @@ class controller_rpgd_me_tf(template_controller):
             )
             traj_cost_mc_estimate = tf.reduce_mean(traj_cost) - self.alpha * self.entropy(theta)
         # retrieve gradient of cost w.r.t. input sequence
-        dc_dQ = tape.gradient(traj_cost_mc_estimate, theta)
-        dc_dQ_prc = tf.clip_by_norm(dc_dQ, self.gradmax_clip)
+        dc_dT = tape.gradient(traj_cost_mc_estimate, theta)
+        dc_dT_prc = tf.clip_by_norm(dc_dT, self.gradmax_clip, axes=-1)
         # use optimizer to apply gradients and retrieve next set of input sequences
-        opt.apply_gradients(zip([dc_dQ_prc], [theta]))
+        opt.apply_gradients(zip([dc_dT_prc], [theta]))
         # clip
         theta = tf.clip_by_value(theta, self.theta_min, self.theta_max)
         return theta, traj_cost
@@ -198,7 +213,8 @@ class controller_rpgd_me_tf(template_controller):
         # Retrieve optimal input and warmstart for next iteration
         u = tf.squeeze(Q[sorted_cost[0], 0, :])
         epsilon_new = tf.concat([epsilon[:, 1:, :], epsilon[:, -1:, :]], axis=1)
-        return u, epsilon_new, best_idx, traj_cost, rollout_trajectory
+        theta_new = tf.concat([theta[:, 1:, :, :], theta[:, -1:, :, :]], axis=1)
+        return u, epsilon_new, theta_new, best_idx, traj_cost, rollout_trajectory
 
     def step(self, s: np.ndarray, time=None):
         if self.controller_logging:
@@ -217,8 +233,8 @@ class controller_rpgd_me_tf(template_controller):
         # optimize control sequences with gradient based optimization
         # prev_cost = tf.convert_to_tensor(np.inf, dtype=tf.float32)
         for _ in range(0, iters):
-            theta, traj_cost = self.grad_step(s, self.epsilon, self.theta, self.opt)
-            self.theta.assign(theta)
+            _theta, traj_cost = self.grad_step(s, self.epsilon, self.theta, self.opt)
+            self.theta.assign(_theta)
 
             # check for convergence of optimization
             # if bool(
@@ -235,15 +251,18 @@ class controller_rpgd_me_tf(template_controller):
         (
             self.u,
             epsilon_new,
+            theta_new,
             best_idx,
             J,
             rollout_trajectory,
         ) = self.get_action(s, self.epsilon, self.theta)
+        self.epsilon = epsilon_new
+        self.theta.assign(theta_new)
         
         self.u = self.u.numpy()
         
         if self.controller_logging:
-            self.current_log["Q_logged"] = self.zeta(self.theta, epsilon_new).numpy()
+            self.current_log["Q_logged"] = self.zeta(self.theta, self.epsilon).numpy()
             self.current_log["J_logged"] = J.numpy()
             self.current_log["rollout_trajectories_logged"] = rollout_trajectory.numpy()
             self.current_log["trajectory_ages_logged"] = self.trajectory_ages.numpy()
@@ -258,7 +277,7 @@ class controller_rpgd_me_tf(template_controller):
             epsilon_res = self.sample_actions(
                 self.rng, self.num_rollouts - self.opt_keep_k
             )
-            epsilon_keep = tf.gather(epsilon_new, best_idx, axis=0)  # resorting according to costs
+            epsilon_keep = tf.gather(self.epsilon, best_idx, axis=0)  # resorting according to costs
             self.epsilon = tf.concat([epsilon_res, epsilon_keep], axis=0)
             self.trajectory_ages = tf.concat([
                 tf.gather(self.trajectory_ages, best_idx, axis=0),
@@ -269,15 +288,15 @@ class controller_rpgd_me_tf(template_controller):
             if len(adam_weights) > 0:
                 w1 = tf.concat(
                     [
-                        adam_weights[1][:, 1:, :],
-                        tf.zeros([1, 1, self.theta.shape[-1]]),
+                        adam_weights[1][:, 1:, :, :],
+                        tf.zeros([1, 1, *self.theta.shape[-2:]]),
                     ],
                     axis=1,
                 )
                 w2 = tf.concat(
                     [
-                        adam_weights[2][:, 1:, :],
-                        tf.zeros([1, 1, self.theta.shape[-1]]),
+                        adam_weights[2][:, 1:, :, :],
+                        tf.zeros([1, 1, *self.theta.shape[-2:]]),
                     ],
                     axis=1,
                 )
@@ -288,15 +307,15 @@ class controller_rpgd_me_tf(template_controller):
                 # if it is not time to reset, all optimizer weights are shifted for a warmstart
                 w1 = tf.concat(
                     [
-                        adam_weights[1][:, 1:, :],
-                        tf.zeros([1, 1, self.theta.shape[-1]]),
+                        adam_weights[1][:, 1:, :, :],
+                        tf.zeros([1, 1, *self.theta.shape[-2:]]),
                     ],
                     axis=1,
                 )
                 w2 = tf.concat(
                     [
-                        adam_weights[2][:, 1:, :],
-                        tf.zeros([1, 1, self.theta.shape[-1]]),
+                        adam_weights[2][:, 1:, :, :],
+                        tf.zeros([1, 1, *self.theta.shape[-2:]]),
                     ],
                     axis=1,
                 )
@@ -307,8 +326,11 @@ class controller_rpgd_me_tf(template_controller):
 
     def controller_reset(self):
         # Adaptive sampling distribution (1, mpc_horizon, dim_theta)
-        initial_theta = np.array([[0.5 * float(self.action_low + self.action_high), float(self.sample_stdev)]])
-        initial_theta = np.tile(initial_theta, (self.mpc_horizon, 1))[np.newaxis]
+        # Theta has shape (1, mpc_horizon, num_actions, num_params_per_action)
+        # initial_theta = np.array([[0.5 * float(self.action_low + self.action_high), float(self.sample_stdev)]])
+        # initial_theta = np.array([[float(self.action_low), float(self.action_high)]])
+        initial_theta = tf.stack([self.action_low, self.action_high], 1)
+        initial_theta = np.tile(initial_theta, (self.mpc_horizon, 1, 1))[np.newaxis]
         if hasattr(self, "theta"):
             self.theta.assign(initial_theta)
         else:
