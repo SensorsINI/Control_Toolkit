e9f681968f40221f5469c8c917d3beae3d038f9a
EnnioEvo <artemisvii@gmail.com>
2023-02-20 15:52:00 +0100

    added cosin cost for pendulum

--- others/cost_forces_interface.py
+++ others/cost_forces_interface.py
@@ -11,9 +11,10 @@ config = yaml.load(
     open(os.path.join("Control_Toolkit_ASF", "config_cost_function.yml"), "r"),
     Loader=yaml.FullLoader,
 )
-cartpole_angle_weight = float(config["cartpole_simulator_batched"]["default"]["angle_weight"])
-cartpole_position_weight = float(config["cartpole_simulator_batched"]["default"]["position_weight"])
 
+
+def pendulum(z, p):
+    return -casadi.cos(z[1])
 
 def continuous_mountaincar(z, p):
     return -casadi.sin(3 * z[1]) / ((z[1] - 1.25) ** 2)
@@ -21,8 +22,19 @@ def continuous_mountaincar(z, p):
 def continuous_mountaincar_approximated(z, p):
     return -1.27*(z[1] + 0.4)**3 - 1.56 * (z[1] + 0.4)**2 + 0.00326758 * (z[1] + 0.4) + 0.322505
 
+def cartpole_weights():
+    cartpole_angle_weight = float(config["cartpole_simulator_batched"]["default"]["angle_weight"])
+    cartpole_position_weight = float(config["cartpole_simulator_batched"]["default"]["position_weight"])
+    return cartpole_angle_weight, cartpole_position_weight
+
 def cartpole_simulator1(z, p):
+    cartpole_angle_weight, cartpole_position_weight = cartpole_weights()
     return -cartpole_angle_weight*casadi.cos(z[1]) + cartpole_position_weight*(z[3] - p[3])**2
 
 def cartpole_simulator2(z, p):
+    cartpole_angle_weight, cartpole_position_weight = cartpole_weights()
     return -cartpole_angle_weight*(z[1]**2) + cartpole_position_weight*(z[3] - p[3])**2
+
+def f1tenth(z, p):
+    from Control_Toolkit_ASF.Cost_Functions.Car.racing_forces import racing_forces
+    racing_forces.get_stage_cost(z[2:], z[:2])
