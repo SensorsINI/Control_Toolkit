6e8ef5157f29206a265522db14f26da377df6653
Tobi Delbruck <todelbru@ethz.ch>
2023-02-21 15:33:40 +0100

    improved logging output to make debug logger light gray, include filename in console logger for logging during control

--- others/get_logger.py
+++ others/get_logger.py
@@ -7,9 +7,11 @@ class CustomFormatter(logging.Formatter):
     """Logging Formatter to add colors and count warning / errors"""
     # see https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output/7995762#7995762
 
-    grey = "\x1b[38;21m"
+    # \x1b[ (ESC[) is the CSI introductory sequence for ANSI https://en.wikipedia.org/wiki/ANSI_escape_code
+    # The control sequence CSI n m, named Select Graphic Rendition (SGR), sets display attributes.
+    grey = "\x1b[2;37m" # 2 faint, 37 gray
     yellow = "\x1b[33;21m"
-    cyan = "\x1b[1;36m" #
+    cyan = "\x1b[1;36m" # 1 bold 36 cyan
     green = "\x1b[31;21m" # dark green
     red = "\x1b[31;21m"
     bold_red = "\x1b[31;1m"
@@ -33,16 +35,15 @@ class CustomFormatter(logging.Formatter):
         return formatter.format(record).replace("\\", "/") #replace \ with / for pycharm links
 
 
-def get_logger(name=None):
+def get_logger(name='ControlToolkit'):
     """ Use get_logger to define a logger with useful color output and info and warning turned on according to the global LOGGING_LEVEL.
 
-    :param name: the name of this logger. Use __name__ to give it the name of the module that instantiates it.
-        If no name is supplied, it defaults to root logger which may turn on a LOT of undesired logging output.
+    :param name: ignored -- all loggers here have the name 'ControlToolkit' so that all can be affected uniformly
 
     :returns: the logger.
     """
     # logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    logger = logging.getLogger(name)
+    logger = logging.getLogger('ControlToolkit') # tobi changed so all have same name so we can uniformly affect all of them
     logger.setLevel(LOGGING_LEVEL)
     # create console handler
     ch = logging.StreamHandler()
