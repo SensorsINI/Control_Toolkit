21cc58d43e4ba77bbf407687c070f7d90d3c612b
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-08-29 11:28:39 +0200

    Changes making gym to run after updating SI_Toolkit

--- Cost_Functions/CostFunctionUpdater.py
+++ Cost_Functions/CostFunctionUpdater.py
@@ -11,34 +11,36 @@ class CostFunctionUpdater:
 
     def __init__(self, cost_function, environment_name, cost_function_name):
 
-        self.config_path = os.path.abspath(cost_function.config_path)
-        # Verify if the configuration file exists
-        if not os.path.isfile(self.config_path):
-            raise FileNotFoundError(f"Configuration file not found at path: {self.config_path}")
+        if hasattr(cost_function, 'config_path'):
+            self.config_path = os.path.abspath(cost_function.config_path)
+            # Verify if the configuration file exists
+            if not os.path.isfile(self.config_path):
+                raise FileNotFoundError(f"Configuration file not found at path: {self.config_path}")
 
-        # If the path is already being watched, stop the previous watcher
-        if self.config_path in CostFunctionUpdater.active_watchers:
-            print(f"Path {self.config_path} is already being watched. Stopping the previous watcher.")
-            CostFunctionUpdater.active_watchers[self.config_path].stop()
+            # If the path is already being watched, stop the previous watcher
+            if self.config_path in CostFunctionUpdater.active_watchers:
+                print(f"Path {self.config_path} is already being watched. Stopping the previous watcher.")
+                CostFunctionUpdater.active_watchers[self.config_path].stop()
 
-        self.observer = Observer()
-        self.handler = ConfigChangeHandler(cost_function, environment_name, cost_function_name)  # Your custom event handler
-        self.cost_config_watch = self.observer.schedule(self.handler, self.config_path, recursive=True)
-        atexit.register(self.stop)
-        self.observer.start()
+            self.observer = Observer()
+            self.handler = ConfigChangeHandler(cost_function, environment_name, cost_function_name)  # Your custom event handler
+            self.cost_config_watch = self.observer.schedule(self.handler, self.config_path, recursive=True)
+            atexit.register(self.stop)
+            self.observer.start()
 
-        # Update the active_watchers dictionary with the new observer
-        CostFunctionUpdater.active_watchers[self.config_path] = self
+            # Update the active_watchers dictionary with the new observer
+            CostFunctionUpdater.active_watchers[self.config_path] = self
 
     def stop(self):
-        if self.observer is not None and self.observer.is_alive():
-            self.observer.stop()
-            self.observer.join()
-            self.observer = None
+        if hasattr(self, 'observer'):  # If not, the CostFunctionUpdater was just cleared
+            if self.observer is not None and self.observer.is_alive():
+                self.observer.stop()
+                self.observer.join()
+                self.observer = None
 
-        # Remove the path from the active_watchers dictionary
-        if self.config_path in CostFunctionUpdater.active_watchers:
-            del CostFunctionUpdater.active_watchers[self.config_path]
+            # Remove the path from the active_watchers dictionary
+            if self.config_path in CostFunctionUpdater.active_watchers:
+                del CostFunctionUpdater.active_watchers[self.config_path]
 
     def __del__(self):
         self.stop()
