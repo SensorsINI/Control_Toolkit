a982789b3c3992a5bd81be26fc18c05f0a12be99
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-05-12 23:42:14 +0200

    fixing random number generator

--- others/globals_and_utils.py
+++ others/globals_and_utils.py
@@ -65,8 +65,8 @@ class torch_gen_like_TF:
         self.torch = torch
         self.rng = torch.Generator().manual_seed(seed)
 
-    def normal(self, shape, dtype):
-        return self.torch.normal(mean=0.0, std=1.0, size=shape, generator=self.rng, dtype=dtype)
+    def normal(self, shape, dtype, mean=0.0, stddev=1.0):
+        return self.torch.normal(mean=mean, std=stddev, size=shape, generator=self.rng, dtype=dtype)
 
 
 def create_rng(id: str, seed: int, computation_library: ComputationLibrary = NumpyLibrary()):
