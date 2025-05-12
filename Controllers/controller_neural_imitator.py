0c156e353d104c56bcda49e3bc1ff38123eaac91
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-05-12 15:16:46 +0200

    Making neural imitator run with Pytorch

--- Controllers/controller_neural_imitator.py
+++ Controllers/controller_neural_imitator.py
@@ -31,7 +31,7 @@ class controller_neural_imitator(template_controller):
         # Prepare input mapping
         self.input_mapping = self._create_input_mapping()
 
-        if self.controller_logging and not self.net_evaluator.hls4ml:
+        if self.controller_logging and self.lib.lib == "TF" and not self.net_evaluator.hls4ml:
             self.controller_data_for_csv = FunctionalDict(get_memory_states(self.net_evaluator.net))
 
         print('Configured neural imitator with {} network with {} library'.format(self.net_evaluator.net_info.net_full_name, self.net_evaluator.net_info.library))
