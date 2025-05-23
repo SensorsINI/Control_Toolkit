ab2c6a4b27e1eb3ba95ed708c3fd7806c635bf94
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-05-23 08:09:02 +0200

    Fixing library delivery to predictor_single_trajectory in mppi

--- Optimizers/optimizer_mppi.py
+++ Optimizers/optimizer_mppi.py
@@ -133,6 +133,7 @@ class optimizer_mppi(template_optimizer):
         self.predictor_single_trajectory.configure(
             batch_size=1, horizon=self.mpc_horizon, dt=dt,  # TF requires constant batch size
             predictor_specification=predictor_specification,
+            computation_library=self.lib,
         )
 
         self.optimizer_reset()
